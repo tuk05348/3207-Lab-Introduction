83a0fc043bd14f2decfe768733ff05d0469d5fdd
Niaz Baharudeen <tuk05348@temple.edu>
2023-01-20 13:11:57 -0500

    Added comments explaining each variable declaration and use of rand in randchar() that led from creating a random integer to converting it into a char.

--- random.c
+++ random.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 char randchar(){
-	int alpha = 25;
-	int start = 65;
-	int num = rand()%alpha + start;
-	char c = itoa(num);
-	return c;
+	int alpha = 25; //Number of letters in alphabet
+	int start = 65;	//Decimal number where alphabetic characters start in the ASCII table
+	int num = rand()%alpha + start; //Use the rand function modulo alpha to get numbers between 0 and 25. Then add to 65 to get numbers corresponding to letters in the ASCII table
+	char c = '0' + num; //To convert the number to a char add '0' in char form
+	return c; //Return the char
 }
