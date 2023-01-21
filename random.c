5b49ce993e1e38c8b552840eb92d8b56565244d6
Niaz Baharudeen <tuk05348@temple.edu>
2023-01-20 22:16:18 -0500

    Corrected an error in converting the random number to a char. Originally had the number added to '0' but the number returned was already a digit within the decimal range of 65 to 90 (the range for the alphabet in ASCII) so all I had to do was typecast the int to a char to convert it as all chars in C are by heart integers.

--- random.c
+++ random.c
@@ -5,6 +5,6 @@ char randchar(){
 	int alpha = 25; //Number of letters in alphabet
 	int start = 65;	//Decimal number where alphabetic characters start in the ASCII table
 	int num = rand()%alpha + start; //Use the rand function modulo alpha to get numbers between 0 and 25. Then add to 65 to get numbers corresponding to letters in the ASCII table
-	char c = '0' + num; //To convert the number to a char add '0' in char form
+	char c = num; //num is already a digit between 65 and 90 thus we can directly convert it to a char
 	return c; //Return the char
 }
